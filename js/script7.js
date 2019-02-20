98d52bc978a382f54be290f10688f1143cffe977
theborgh <borghiborghi@gmail.com>
2019-02-20 12:50:53 +0100

    Add script7.js script8.js

---
+++ js/script7.js
@@ -0,0 +1,68 @@
+// REFERENCE TYPE
+
+[] === []; // false (they're 2 different objects)
+
+let obj1 = {age: 10};
+let obj2 = obj1;
+let obj2 = {age: 10};
+
+obj1 === obj2 // true
+obj1 === obj3 // false
+
+obj2.age = 13;
+obj1.age // 13
+
+// CONTEXT
+
+const obj = {
+    a: function() {
+        console.log(this);
+    },
+	b: "otherfield"
+}
+
+// DO NOT write methods using arrow functions: the `this` within an arrow function!
+// Arrow functions close over the `this` of the LEXICALLY ENCLOSING CONTEXT,
+// so the `this` within the arrow function is the one that was current where I defined the object.
+// Arrow functions ARE NOT just a shorthand for function()!
+// Do not use them for methods or constructors
+// They have THEIR OWN BINDINGS to the this, arguments, super, and new.target keywords
+const obj2 = {
+    a: () => {
+        console.log(this); 
+    },
+	b: "otherfield"
+}
+
+obj.a() // obj ("this" is the object that is calling a(); the context of a())
+
+obj2.a() // window
+
+// INSTANTIATION
+
+class Player {
+    constructor(name, type) {
+        this.name = name; // I don't need to declare class fields!
+        this.type = type;
+    }
+
+    introduce() {
+        console.log(`Hi, I'm ${this.name} of type ${this.type}`);
+    }
+}
+
+let p = new Player("gaga", "wizard");
+p.introduce();
+
+class Wizard extends Player {
+    spells = ["fireball", "invisibility"];
+
+    constructor(name, type, spell) {
+        super(name, type);
+        this.spell = spell;
+    }
+
+    cast(i) {
+        console.log(`Casting the ${spells[i]} spell!`);
+    }
+}
---
+++ js/script8.js
@@ -0,0 +1,65 @@
+// ES7
+// .includes()
+
+"hello".includes("o"); // true
+"hello".includes("e", 1); // true
+"hello".includes("e", 2); // false
+
+const pets = ["cat", "dog", "snake"];
+pets.includes("snake") // true
+pets.includes("caterpillar") // false
+
+// Exponential operator
+
+2 ** 8 // 256
+
+// ES8
+// String padding
+
+"hello".padStart(5); // "hello"
+"hello".padStart(10); // "     hello"
+"hello".padEnd(10); // "hello     "
+
+// trailing commas in function parameter list and calls
+
+const f = (a, b, c, d,) => a-b+c*d 
+f(1, 4, 5, 1) // this works
+f(1, 4, 5, 1,) // also works
+
+// .values() and .entries()
+
+const obj = {
+    name1: "pippo",
+    name2: "geordie",
+}
+
+Object.keys(obj).forEach((key) => {
+    console.log(key, obj[key]);
+});
+
+Object.entries(obj).forEach(e => console.log(e)) // all entries and values
+
+Object.values(obj).forEach(v => console.log(v)); // all values
+
+const obj1 = Object.entries(obj).map(e => {
+    return e[1] + e[0].replace("name", "");
+});
+
+const obj2 = Object.entries(obj).map((e, i) => {
+    let tmp = e;
+    tmp[0] = i;
+    return tmp;
+})
+
+let obj3 = {
+    my: 'name',
+    is: 'Rudolf',
+    the: 'reindeer'
+}
+
+// with map/reduce
+let s = Object.entries(obj3).map(e => `${e[0]} ${e[1]}`)
+                            .reduce((acc, curr) => `${acc} ${curr}`);
+
+// easier way
+let s1 = Object.entries(obj3).map(value => value.join(" ")).join(" ");
