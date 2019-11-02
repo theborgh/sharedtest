af767bb57f0e1a13b52f25461864c7da3045e33e
theborgh <borghiborghi@gmail.com>
2019-11-02 21:30:50 +0100

    Add projects file, pass props to Project component

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -2,18 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Project.scss";
 
-const Project = ({ name, description, tech }) => {
+const Project = ({
+  name,
+  description,
+  techStack,
+  screenshotUrl,
+  repositoryUrl,
+  liveUrl
+}) => {
   Project.propTypes = {
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
-    tech: PropTypes.array.isRequired
+    techStack: PropTypes.array.isRequired,
+    screenshotUrl: PropTypes.string.isRequired,
+    repositoryUrl: PropTypes.string.isRequired,
+    liveUrl: PropTypes.string.isRequired
   };
 
   return (
-    <div className="Project">
+    <div
+      className="Project"
+      style={{ background: `url(${screenshotUrl})`, backgroundSize: "cover" }}
+    >
       Name: {name}
       Description: {description}
-      Tech: {tech ? tech.map(t => `${t} `) : null}
+      Tech: {techStack ? techStack.map(t => `${t} `) : null}
     </div>
   );
 };
---
+++ src/pages/ProjectsPage/projects.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    name: "Palette Maker",
+    description: "",
+    techStack: ["React.js", "JSS", "Material-UI"],
+    screenshotUrl: "",
+    repositoryUrl: "",
+    liveUrl: ""
+  },
+
+  {
+    name: "The Dad Jokes App",
+    description: "",
+    techStack: ["React.js"],
+    screenshotUrl: "",
+    repositoryUrl: "",
+    liveUrl: ""
+  },
+
+  {
+    name: "Guess the Word",
+    description: "",
+    techStack: ["React.js", "Jest", "Enzyme"],
+    screenshotUrl: "",
+    repositoryUrl: "",
+    liveUrl: ""
+  },
+
+  {
+    name: "Portfolio",
+    description: "",
+    techStack: ["React.js"],
+    screenshotUrl: "",
+    repositoryUrl: "",
+    liveUrl: ""
+  }
+];
