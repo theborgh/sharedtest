d7ba0fe3d5569fcd87979f530e7611a902442f83
theborgh <borghiborghi@gmail.com>
2020-11-01 03:11:44 +0100

    fix

--- src/pages/ContactPage/ContactForm/ContactForm.jsx
+++ src/pages/ContactPage/ContactForm/ContactForm.jsx
@@ -25,70 +25,84 @@ const ContactForm = () => {
         message
       })
     })
-      .then(() => setEmailSent(true))
+      .then(response => {
+        if (response.status === 200) {
+          setEmailSent(true);
+        } else {
+          setEmailError(true);
+        }
+      })
       .catch(() => setEmailError(true));
 
     e.preventDefault();
   };
 
   return (
-    <form
-      className="contactform"
-      name="contact-form"
-      onSubmit={handleFormSubmit}
-    >
-      <div className="contactform-input">
-        <label htmlFor="name">
-          <span className="contactform-labeltext">Your name</span>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            placeholder="John Doe"
+    <div>
+      <form name="contact-form" netlify netlify-honeypot="bot-field" hidden>
+        <input type="text" name="name" />
+        <input type="email" name="email" />
+        <textarea name="message" />
+      </form>
+
+      <form
+        className="contactform"
+        name="contact-form"
+        onSubmit={handleFormSubmit}
+      >
+        <div className="contactform-input">
+          <label htmlFor="name">
+            <span className="contactform-labeltext">Your name</span>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              placeholder="John Doe"
+              required
+            />
+          </label>
+        </div>
+        <div className="contactform-input">
+          <label htmlFor="email">
+            <span className="contactform-labeltext">Your email</span>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              placeholder="your@email.com"
+              required
+            />
+          </label>
+        </div>
+        <div className="contactform-input">
+          <textarea
+            name="message"
+            id="message"
+            cols="80"
+            rows="20"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+            placeholder="Your message here"
             required
           />
-        </label>
-      </div>
-      <div className="contactform-input">
-        <label htmlFor="email">
-          <span className="contactform-labeltext">Your email</span>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            placeholder="your@email.com"
-            required
-          />
-        </label>
-      </div>
-      <div className="contactform-input">
-        <textarea
-          name="message"
-          id="message"
-          cols="80"
-          rows="20"
-          value={message}
-          onChange={e => setMessage(e.target.value)}
-          placeholder="Your message here"
-          required
-        />
-      </div>
+        </div>
 
-      <div className="submit-container">
-        <button className="submit-button" type="submit">
-          Send
-        </button>
-        {emailSent && <div className="email-success">Email sent!</div>}
+        <div className="submit-container">
+          <button className="submit-button" type="submit">
+            Send
+          </button>
+          {emailSent && <div className="email-success">Email sent!</div>}
 
-        {emailError && (
-          <div className="email-error">Email could not be sent</div>
-        )}
-      </div>
-    </form>
+          {emailError && (
+            <div className="email-error">Email could not be sent</div>
+          )}
+        </div>
+      </form>
+    </div>
   );
 };
 
