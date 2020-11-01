eb12662457edb484cedb86ced4107016e70c0e5c
theborgh <borghiborghi@gmail.com>
2020-11-01 04:03:05 +0100

    Add error msg color, disable UI after first attempt

--- src/pages/ContactPage/ContactForm/ContactForm.jsx
+++ src/pages/ContactPage/ContactForm/ContactForm.jsx
@@ -38,66 +38,79 @@ const ContactForm = () => {
   };
 
   return (
-    <div>
-      <form
-        className="contactform"
-        name="contact-form"
-        onSubmit={handleFormSubmit}
-      >
-        <input type="hidden" name="form-name" value="contact-form" />
-        <div className="contactform-input">
-          <label htmlFor="name">
-            <span className="contactform-labeltext">Your name</span>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={name}
-              onChange={e => setName(e.target.value)}
-              placeholder="John Doe"
-              required
-            />
-          </label>
-        </div>
-        <div className="contactform-input">
-          <label htmlFor="email">
-            <span className="contactform-labeltext">Your email</span>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-              placeholder="your@email.com"
-              required
-            />
-          </label>
-        </div>
-        <div className="contactform-input">
-          <textarea
-            name="message"
-            id="message"
-            cols="80"
-            rows="20"
-            value={message}
-            onChange={e => setMessage(e.target.value)}
-            placeholder="Your message here"
+    <form
+      className="contactform"
+      name="contact-form"
+      onSubmit={handleFormSubmit}
+    >
+      <input type="hidden" name="form-name" value="contact-form" />
+      <div className="contactform-input">
+        <label htmlFor="name">
+          <span className="contactform-labeltext">Your name</span>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            placeholder="John Doe"
+            disabled={emailSent || emailError}
             required
           />
-        </div>
+        </label>
+      </div>
+      <div className="contactform-input">
+        <label htmlFor="email">
+          <span className="contactform-labeltext">Your email</span>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            placeholder="your@email.com"
+            disabled={emailSent || emailError}
+            required
+          />
+        </label>
+      </div>
+      <div className="contactform-input">
+        <textarea
+          name="message"
+          id="message"
+          cols="80"
+          rows="20"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+          placeholder="Your message here"
+          disabled={emailSent || emailError}
+          required
+        />
+      </div>
 
-        <div className="submit-container">
-          <button className="submit-button" type="submit">
-            Send
-          </button>
-          {emailSent && <div className="email-success">Email sent!</div>}
+      <div className="submit-container">
+        <button
+          className="submit-button"
+          type="submit"
+          disabled={emailSent || emailError}
+        >
+          Send
+        </button>
+        {emailSent && (
+          <div className="email-success">
+            Email sent! Thank you for contacting me.
+          </div>
+        )}
 
-          {emailError && (
-            <div className="email-error">Email could not be sent</div>
-          )}
-        </div>
-      </form>
-    </div>
+        {/* TODO: notify me when someone gets something other than 200 status response */}
+        {emailError && (
+          <div className="email-error">
+            Unfortunately there was a network error and your message could not
+            be sent.
+          </div>
+        )}
+      </div>
+    </form>
   );
 };
 
